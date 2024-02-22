2e4b1fa4283ad654a5d3ff7fdc4e2d68999b948e
Philani Ncube <ncbphi001@gmail.com>
2024-02-22 13:26:50 +0200

    Add redirect to dashboard if user is logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import SignIn from "@/components/auth/sign-in";
 import SignUp from "@/components/auth/sign-up";
 import { createClient } from "@/utils/supabase/server";
+
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 
 export default async function Home() {
@@ -14,11 +16,13 @@ export default async function Home() {
 			error,
 		} = await supabase.auth.getUser();
 
+    if (user) redirect("/dashboard");
+
 
 
   return (
 			<main className="flex min-h-screen flex-col items-center justify-between p-24">
-				{!user ? <SignIn /> : <p>Welcome {user.email}</p>}
+				{!user && <SignIn />}
 			</main>
 		);
 }
--- middleware.ts
+++ middleware.ts
@@ -55,7 +55,7 @@ export async function middleware(request: NextRequest) {
     }
   )
 
-  await supabase.auth.getUser()
+
 
   return response
 }
